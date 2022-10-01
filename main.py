271bf84617e7fb7d7e08049df7ce56e704f3d871
Alex Porojan <aporojan@ryerson.ca>
2022-10-01 12:33:51 -0400

    encapsulated screen updates into DrawFrame() function and added capability for recognizing when a controller disconnects or when no controller is connected and displaying a message accordingly

--- main.py
+++ main.py
@@ -27,17 +27,7 @@ from asyncio.windows_events import NULL
 from math import ceil
 import pygame
 import socket
-
-# Controller and screen variables
-BLACK = pygame.Color('black')
-WHITE = pygame.Color('white')
-ACTUAL_BUTTONS_IN_USE = 10
-FPS = 60.0
-FRAME_EVERY_X_MILLIS = ceil(1000.0 / FPS)
-buttonMap = ["A", "B", "X", "Y", "LB", "RB", "BACK", "START", "LS", "RS"]
-axisMap = ["LS Left-Right", "LS Up-Down", "RS Left-Right", "RS Up-Down", "LT", "RT"]
-dPadMap = ["right", "down"]
-DPAD_UP, DPAD_DOWN, DPAD_LEFT, DPAD_RIGHT = [0 for i in range(4)]
+debug = False
 
 # Simple class to print to the screen in pygame
 class TextPrint(object):
@@ -61,31 +51,119 @@ class TextPrint(object):
 	def unindent(self):
 		self.x -= 10
 
-# Initialize pygame library and controllers
+# Function to update the screen
+def DrawFrame():
+	global frame_time, joystick
+
+	frame_time = 0
+	screen.fill(WHITE)
+	textPrint.reset()
+
+	if joystick_count == 0:
+		textPrint.tprint(screen, "No Joystick detected!")
+
+	else:
+		textPrint.tprint(screen, "Number of joysticks: {}".format(joystick_count))
+		textPrint.indent()
+
+		textPrint.tprint(screen, "Joystick: {}".format(jid))
+		textPrint.indent()
+
+		textPrint.tprint(screen, "Joystick name: {}".format(name))
+		textPrint.tprint(screen, "GUID: {}".format(guid))
+
+		# Check axis movement
+		axes = joystick.get_numaxes()
+		textPrint.tprint(screen, "Number of axes: {}".format(axes))
+		textPrint.indent()
+
+		for i in range(axes):
+			axis = joystick.get_axis(i)
+			textPrint.tprint(screen, "Axis {} value: {:>6.3f}".format(axisMap[i], axis))
+		textPrint.unindent()
+
+		# Check button press/release
+		buttons = joystick.get_numbuttons()
+		textPrint.tprint(screen, "Number of buttons: {}".format(buttons))
+		textPrint.indent()
+
+		for i in range(ACTUAL_BUTTONS_IN_USE):
+			button = joystick.get_button(i)
+			textPrint.tprint(screen, "{} value: {}".format(buttonMap[i], button))
+		textPrint.unindent()
+
+		# Check D-pad button press/release
+		hats = joystick.get_numhats()
+		textPrint.tprint(screen, "Number of hats: {}".format(hats))
+		textPrint.indent()
+
+		# Hat position is a tuple of int values (x, y)
+		for i in range(hats):
+			hat = joystick.get_hat(i)
+			if hat[0] == 1: dPadMap[0] = "right"
+			elif hat[0] == -1: dPadMap[0] = "left"
+			else: dPadMap[0] = ""
+			if hat[1] == 1: dPadMap[1] = "up"
+			elif hat[1] == -1: dPadMap[1] = "down"
+			else: dPadMap[1] = ""
+			textPrint.tprint(screen, "DPAD horizontal value: {} {}".format(str(hat), dPadMap[0]))
+			textPrint.tprint(screen, "DPAD vertical value: {} {}".format(str(hat), dPadMap[1]))
+		
+		textPrint.unindent()
+		textPrint.unindent()
+	
+	# Update the screen
+	pygame.display.flip()
+
+	# Record millis passed since last call to clock.tick()
+	frame_time = clock.tick()
+
+# Screen variables
+BLACK = pygame.Color('black')
+WHITE = pygame.Color('white')
+ACTUAL_BUTTONS_IN_USE = 10
+FPS = 60.0
+FRAME_EVERY_X_MILLIS = ceil(1000.0 / FPS)
+
+# Controller variables
+buttonMap = ["A", "B", "X", "Y", "LB", "RB", "BACK", "START", "LS", "RS"]
+buttonValues = {buttonMap[i]:False for i in range(len(buttonMap))}
+if debug: print(buttonValues)
+
+axisMap = ["LS Left-Right", "LS Up-Down", "RS Left-Right", "RS Up-Down", "LT", "RT"]
+axisValues = {axisMap[i]:0.0 if i<(len(axisMap)-2) else -1 for i in range(len(axisMap))}
+if debug: print(axisValues)
+
+dPadMap = ["right", "down"]
+DPAD_UP, DPAD_DOWN, DPAD_LEFT, DPAD_RIGHT = [0 for i in range(4)]
+
+# Initialize pygame library, joystick module, clock, frame time tracking
 pygame.init()
 pygame.joystick.init()
-
-# Used to manage how fast the screen updates
+joystick_count = 0
 clock = pygame.time.Clock()
 frame_time = 0
 
-# Set the width and height of the screen (width, height).
+# Set the width and height of the screen as (width, height)
+#	and initialize textPrint object to display text in pygame
 screen = pygame.display.set_mode((500, 500))
 pygame.display.set_caption("Controller Input")
-
-# Initialize textPrint object to display text in pygame
 textPrint = TextPrint()
 
-# Loop until the user clicks the close button.
-done = False
-
 # Actuator variables
-leftDriveModeOptions = [0, 1, 2, 3, 6]
+leftDriveModeOption = [0, 1, 2, 3, 6]
+leftDriveModeOptionIndex = 0
 leftDriveMode = 0
-rightDriveModeOptions = [0, 1, 2, 3, 6]
+
+rightDriveModeOption = [0, 1, 2, 3, 6]
+rightDriveModeOptionIndex = 0
 rightDriveMode = 0
+
 upperExtender, lowerExtender, screwdriver, claw, hoist, swivel = [0 for i in range(6)]
 leftWheel1, rightWheel1, leftWheel2, rightWheel2, leftWheel3, rightWheel3 = [0 for i in range(6)]
+
+# Loop until the user clicks the close button
+done = False
 
 # Controller Input Loop
 while not done:
@@ -94,109 +172,46 @@ while not done:
 	# event.type == pygame.JOYBUTTONDOWN: Joystick button pressed
 	# event.type == pygame.JOYBUTTONUP: Joystick button released
 
+	# Read events waiting since last loop
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT: # User closed the window
 			done = True
+
 		elif event.type == pygame.JOYDEVICEREMOVED: # Controller connection lost
 			joystick_count = pygame.joystick.get_count()
-			print("Controller disconnected!\nRemaining controllers connected: %d"\
-				%joystick_count)
+			print("Controller disconnected!")
+			print("Remaining controllers connected: {}".format(joystick_count))
 			if not joystick_count:
-				# Prompt user to continue or exit maybe?
-				done = True
-				continue
+				# Prompt user to wait or exit maybe?
+				print("\t What to do?")
+
 		elif event.type == pygame.JOYDEVICEADDED: # New controller detected
-			# Get count of joysticks
 			joystick_count = pygame.joystick.get_count()
-			controller_num = joystick_count - 1
+
+			# Wait and do nothing if no controllers detected
+			if joystick_count == NULL: continue
+			else: controller_num = joystick_count - 1
 
 			joystick = pygame.joystick.Joystick(controller_num)
 			joystick.init()
-
-			try:
-				jid = joystick.get_instance_id()
-			except AttributeError:
-				# get_instance_id() is an SDL2 method
-				jid = joystick.get_id()
-			
-			# Get the name from the OS for the controller
 			name = joystick.get_name()
 
-			try:
-				guid = joystick.get_guid()
-			except AttributeError:
-				# get_guid() is an SDL2 method
-				pass
-		
+			try: jid = joystick.get_instance_id() # get_instance_id() is an SDL2 method
+			except AttributeError: jid = joystick.get_id()
+
+			try: guid = joystick.get_guid() # get_guid() is an SDL2 method
+			except AttributeError: pass
+
 
 	# Check to see if it's time to draw another frame
 	frame_time += clock.tick()
-	if frame_time < FRAME_EVERY_X_MILLIS:
-		continue
- 
-	# START DRAWING
-	frame_time = 0
-	screen.fill(WHITE)
-	textPrint.reset()
+	if frame_time < FRAME_EVERY_X_MILLIS: continue
 
-	textPrint.tprint(screen, "Number of joysticks: {}".format(joystick_count))
-	textPrint.indent()
-
-	textPrint.tprint(screen, "Joystick {}".format(jid))
-	textPrint.indent()
-
-	textPrint.tprint(screen, "Joystick name: {}".format(name))
-	textPrint.tprint(screen, "GUID: {}".format(guid))
-
-	# Check axis movement
-	axes = joystick.get_numaxes()
-	textPrint.tprint(screen, "Number of axes: {}".format(axes))
-	textPrint.indent()
-
-	for i in range(axes):
-		axis = joystick.get_axis(i)
-		textPrint.tprint(screen, "Axis {} value: {:>6.3f}".format(axisMap[i], axis))
-	textPrint.unindent()
-
-	# Check button press/release
-	buttons = joystick.get_numbuttons()
-	textPrint.tprint(screen, "Number of buttons: {}".format(buttons))
-	textPrint.indent()
-
-	for i in range(ACTUAL_BUTTONS_IN_USE):
-		button = joystick.get_button(i)
-		textPrint.tprint(screen, "{} value: {}".format(buttonMap[i], button))
-	textPrint.unindent()
-
-	# Check D-pad button press/release
-	hats = joystick.get_numhats()
-	textPrint.tprint(screen, "Number of hats: {}".format(hats))
-	textPrint.indent()
-
-	# Hat position is a tuple of int values (x, y)
-	for i in range(hats):
-		hat = joystick.get_hat(i)
-		if hat[0] == 1: dPadMap[0] = "right"
-		elif hat[0] == -1: dPadMap[0] = "left"
-		else: dPadMap[0] = ""
-		if hat[1] == 1: dPadMap[1] = "up"
-		elif hat[1] == -1: dPadMap[1] = "down"
-		else: dPadMap[1] = ""
-		textPrint.tprint(screen, "DPAD horizontal value: {} {}".format(str(hat), dPadMap[0]))
-		textPrint.tprint(screen, "DPAD vertical value: {} {}".format(str(hat), dPadMap[1]))
-	textPrint.unindent()
-	textPrint.unindent()
-
-	# STOP DRAWING
-	# Update the screen
-	pygame.display.flip()
- 
-	# Record millis passed since last call to clock.tick()
-	frame_time = clock.tick()
+	DrawFrame()
 
 	# Send control packets over socket connection
 	# print("DriveCommand_{}_{}_{}_{}_{}_{}".format(leftWheel1, rightWheel1, leftWheel2, rightWheel2, leftWheel3, rightWheel3))
 	# print("ArmCommand_{}_{}_{}_{}_{}_{}".format(upperExtender, lowerExtender, screwdriver, claw, hoist, swivel))
 
-# Close the window and quit.
+# Close the window and quit
 pygame.quit()
